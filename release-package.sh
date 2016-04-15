c8e2a850750a3d9253c4ae7a8d801cc3b5b194f7
Prabhat Jha <prabhat143@gmail.com>
2016-04-15 13:28:49 -0700

    quick script to create a zip for download

---
+++ release-package.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+VERSION=2.0.0
+PRODUCT_NAME=apigee-edge-micro
+DIR_TO_CREATE="$HOME/$PRODUCT_NAME-$VERSION"
+SOURCE_DIR=`pwd`
+echo "Copying source to temp directory $DIR_TO_CREATE"
+cp -r . $DIR_TO_CREATE
+pushd $DIR_TO_CREATE
+npm i --production
+popd
+pushd $HOME
+echo "creating  zip"
+zip -r -q apigee-edge-micro.zip $PRODUCT_NAME-$VERSION -x "*.git*"
+popd
+echo "Deleting the temp directory"
+rm -rf $DIR_TO_CREATE
+mv $HOME/apigee-edge-micro.zip .
+echo 'Done. Get the zipfile :  apigee-edge-micro.zip'
