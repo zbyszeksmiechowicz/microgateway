df1057a1b1111a8c2595e6437835b6f93fdfd145
Shawn Feldman <shawn@Shawns-MacBook.local>
2016-05-25 14:38:21 -0600

    package without git

--- release-package.sh
+++ release-package.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-VERSION=2.0.0
+VERSION=2.0.4
 PRODUCT_NAME=apigee-edge-micro
 DIR_TO_CREATE="$HOME/$PRODUCT_NAME-$VERSION"
 SOURCE_DIR=`pwd`
 echo "Copying source to temp directory $DIR_TO_CREATE"
 cp -r . $DIR_TO_CREATE
 pushd $DIR_TO_CREATE
+rm -rf tests*
+rm -rf .git
+rm -f .gitignore
+rm -rf .idea
+rm -rf .travis.yml
+rm -rf .vscode
 npm i --production
 popd
 pushd $HOME
