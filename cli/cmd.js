0672eba903d8aae85bbbb78e8a4fd42b2df04646
Matthew Dobson <mdobson4@gmail.com>
2017-02-17 10:09:26 -0500

    Removing unused code files.

--- cli/cmd.js
+++ cli/cmd.js
@@ -1,73 +1,13 @@
 'use strict';
 
 const commander = require('commander');
-// const configure = require('./lib/configure')();
-// const verify = require('./lib/verify')();
 const run = require('./lib/gateway')();
-// const keyGenerator = require('./lib/key-gen')();
-// const prompt = require('cli-prompt');
-// const init = require('./lib/init');
-// var portastic = require('portastic');
+
 
 const setup = function setup() {
   commander
     .version(require('../package.json').version);
-  // commander
-  //   .command('token [action]', 'JWT token commands, see: "edgemicro token -h"')
-  //   .command('cert [action]', 'ssh cert commands to store on Apigee Vault, see: "edgemicro cert -h"')
-  //   .command('private [action]', 'Automated, one-time configuration with Edge On-Premises, see: "edgemicro private -h"');
-
-
-  // commander
-  //   .command('configure')
-  //   .description('Automated, one-time configuration with Edge Cloud')
-  //   .option('-o, --org <org>', 'the organization')
-  //   .option('-e, --env <env>', 'the environment')
-  //   .option('-v, --virtualHosts <virtualHosts>', 'override virtualHosts (default: "default,secure")')
-  //   .option('-u, --username <user>', 'username of the organization admin')
-  //   .option('-p, --password <password>', 'password of the organization admin')
-  //   .option('-r, --url <url>', 'organization\'s custom API URL (https://api.example.com)')
-  //   .option('-d, --debug', 'execute with debug output')
-  //   .option('-c, --configDir <configDir>', 'Set the directory where configs are written.')
-  //   .action((options) => {
-  //     options.error = optionError;
-  //     if (!options.username) { return options.error('username is required'); }
-  //     if (!options.org) { return options.error('org is required'); }
-  //     if (!options.env) { return options.error('env is required'); }
-  //     options.configDir = options.configDir || process.env.EDGEMICRO_CONFIG_DIR;
-  //     promptForPassword(options,(options)=>{
-  //       if (!options.password) { return options.error('password is required'); }
-  //       configure.configure(options, () => {
-  //       });
-  //     })
-  //   });
-
-//  commander
-//     .command('init')
-//     .description('initialize default.yaml into home dir')
-//     .option('-c, --configDir <configDir>', 'Set the directory where configs are written.') 
-//     .action((options) => {
-//       options.configDir = options.configDir || process.env.EDGEMICRO_CONFIG_DIR;
-//       init(options, (err,location)=>{
-//         console.log("config initialized to %s",location)
-//       })
-//     });
-
-//   commander
-//     .command('verify')
-//     .description('verify Edge Micro configuration by testing config endpoints')
-//     .option('-o, --org <org>', 'the organization')
-//     .option('-e, --env <env>', 'the environment')
-//     .option('-k, --key <key>', 'key for authenticating with Edge')
-//     .option('-s, --secret <secret>', 'secret for authenticating with Edge')
-//     .action((options) => {
-//       options.error = optionError;
-//       if (!options.org) { return  options.error('org is required'); }
-//       if (!options.env) { return  options.error('env is required'); }
-//       if (!options.key) { return  options.error('key is required'); }
-//       if (!options.secret) { return  options.error('secret is required'); }
-//       verify.verify(options);
-//     });
+  
 
 
   commander
@@ -85,65 +25,8 @@ const setup = function setup() {
       options.systemConfigPath = options.systemConfigPath || process.env.EDGEMICRO_SYSTEM_CONFIG_PATH;
       options.apidEndpoint = options.apidEndpoint || process.env.EDGEMICRO_APID_ENDPOINT;
 
-      // TODO once apid API is changed to no longer need env, this can go away
-      process.env.ENV = options.env;
       run.start(options);
     });
-
-//   commander
-//     .command('reload')
-//     .option('-o, --org <org>', 'the organization')
-//     .option('-e, --env <env>', 'the environment')
-//     .option('-k, --key <key>', 'key for authenticating with Edge')
-//     .option('-s, --secret <secret>', 'secret for authenticating with Edge')
-//     .description('reload the edgemicro cluster by pulling new configuration')
-//     .action((options)=> {
-//       options.error = optionError;
-//       options.secret = options.secret || process.env.EDGEMICRO_SECRET;
-//       options.key = options.key || process.env.EDGEMICRO_KEY;
-//       options.org = options.org || process.env.EDGEMICRO_ORG;
-//       options.env = options.env || process.env.EDGEMICRO_ENV;
-//       if (!options.key ) {return  options.error('key is required');}
-//       if (!options.secret ) {return  options.error('secret is required');}
-//       if (!options.org ) { return  options.error('org is required'); }
-//       if (!options.env ) { return  options.error('env is required'); }
-//       run.reload(options);
-//     });
-
-//   commander
-//     .command('stop')
-//     .description('stop the edgemicro cluster')
-//     .action((options)=> {
-//       run.stop(options);
-//     });
-
-//   commander
-//     .command('status')
-//     .description('Status of the edgemicro cluster')
-//     .action((options)=> {
-//       run.status(options);
-//     });
-
-// commander
-//   .command('genkeys')
-//   .option('-o, --org <org>', 'the organization')
-//   .option('-e, --env <env>', 'the environment')
-//   .option('-u, --username <user>', 'username of the organization admin')
-//   .option('-p, --password <password>', 'password of the organization admin')
-//   .description('generate authentication keys for runtime auth between Microgateway and Edge')
-//   .action((options)=>{
-//     options.error = optionError;
-//     if (!options.username) { return options.error('username is required'); }
-//     if (!options.org) { return options.error('org is required'); }
-//     if (!options.env) { return options.error('env is required'); }
-//     promptForPassword(options,(options)=>{
-//       if (!options.password) { return options.error('password is required'); }
-//       keyGenerator.generate(options,(err)=>{
-//         err ? process.exit(1) : process.exit(0);
-//       });
-//     })
-
-//   });
 
   commander.parse(process.argv);
 
@@ -163,19 +46,5 @@ function optionError(message) {
   console.error(message);
   this.help();
 }
-
-// prompt for a password if it is not specified
-function promptForPassword( options, cb) {
-
-  if (options.password) {
-    cb(options);
-  } else {
-    prompt.password("password:", function(pw) {
-      options.password = pw;
-      cb(options);
-    });
-  }
-}
-
 
 module.exports = setup;
--- edge/README.md
+++
@@ -1,3 +0,0 @@
-#edge dependency directories
-these files are for the cli configure command
-they are deployed to create the edge-auth proxy
