7400f324b92e3ed180df27440ed20dcdb926704d
Nandan Sridhar <srinandans@nandanks.mtv.corp.google.com>
2017-05-10 15:21:27 -0700

    allow cpus to be set via env var

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ options.key = process.env.EDGEMICRO_KEY;
 options.secret = process.env.EDGEMICRO_SECRET;
 options.org = process.env.EDGEMICRO_ORG;
 options.configDir = process.env.EDGEMICRO_CONFIG_DIR;
-options.processes = os.cpus().length;
+options.processes = process.env.EDGEMICRO_PROCESSES || os.cpus().length;
 options.port = process.env.PORT || 8000;
 
 run.start(options);
