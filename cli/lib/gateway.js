afe2093d5123fe34979e8f30cc892d8702e57ba2
Nandan Sridhar <srinandans@google.com>
2017-11-11 15:48:25 -0800

    remove console log

--- cli/lib/gateway.js
+++ cli/lib/gateway.js
@@ -171,7 +171,7 @@ Gateway.prototype.start =  (options) => {
 };
 
 Gateway.prototype.reload = (options) => {
-console.log(options);
+
   const source = configLocations.getSourcePath(options.org, options.env, options.configDir);
   const cache = configLocations.getCachePath(options.org, options.env, options.configDir);
   const keys = {key: options.key, secret: options.secret};
