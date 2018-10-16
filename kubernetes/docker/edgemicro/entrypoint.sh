95308dacfdee412111472a510ea9525eb4de2d49
Nandan Sridhar <srinandans@google.com>
2018-10-15 22:16:23 -0700

    update entrypoint

--- kubernetes/docker/edgemicro/entrypoint.sh
+++ kubernetes/docker/edgemicro/entrypoint.sh
@@ -10,14 +10,30 @@ echo "Log Location should be: [ $LOG_LOCATION ]"
 
 SERVICE_NAME=$(env | grep POD_NAME=| cut -d '=' -f2| cut -d '-' -f1 | tr '[a-z]' '[A-Z]')
 
+if [[ ${CONTAINER_PORT} != "" ]]; then
+    SERVICE_PORT=${CONTAINER_PORT}
+else
+  ## We should create a Service name label if the deployment name is not same as service name
+  ## In most of the cases it will work. The workaround is to add a containerPort label
+  
+  SERVICE_PORT=$(env | grep ${SERVICE_NAME}_SERVICE_PORT_HTTP=| cut -d '=' -f 2)
+fi
+
+proxy_name=edgemicro_${SERVICE_NAME}
+target_port=$SERVICE_PORT
+base_path=/
+processes=""
+background=" &"
+mgstart=" edgemicro start -o $EDGEMICRO_ORG -e $EDGEMICRO_ENV -k $EDGEMICRO_KEY -s $EDGEMICRO_SECRET -d /opt/apigee/plugins "
+localproxy=" export EDGEMICRO_LOCAL_PROXY=$EDGEMICRO_LOCAL_PROXY "
+mgdir="cd /opt/apigee "
+decorator=" export EDGEMICRO_DECORATOR=$EDGEMICRO_DECORATOR "
+debug=" export DEBUG=$DEBUG "
+
 if [[ ${EDGEMICRO_CONFIG} != "" ]]; then
-	echo ${EDGEMICRO_CONFIG} >> /tmp/test.txt
+	#echo ${EDGEMICRO_CONFIG} >> /tmp/test.txt
 	echo ${EDGEMICRO_CONFIG} | base64 -d > /opt/apigee/.edgemicro/$EDGEMICRO_ORG-$EDGEMICRO_ENV-config.yaml
-	# Decorate Proxy with the proxy name
-  sed -i.bak s/proxy_name/${proxy_name}/g /tmp/proxies.yaml
-  if [[ ${EDGEMICRO_DECORATOR} != "" ]]; then
-         sed -i.bak '/edgemicro:/r /tmp/proxies.yaml' /opt/apigee/.edgemicro/$EDGEMICRO_ORG-$EDGEMICRO_ENV-config.yaml
-  fi
+
   chown apigee:apigee /opt/apigee/.edgemicro/*
 fi
 
@@ -29,42 +45,40 @@ if [[ -n "$EDGEMICRO_OVERRIDE_edgemicro_config_change_poll_interval" ]]; then
 fi
 
 if [[ ${EDGEMICRO_PROCESSES} != "" ]]; then
-  commandString="cd /opt/apigee && export EDGEMICRO_DECORATOR=$EDGEMICRO_DECORATOR && edgemicro start -p $EDGEMICRO_PROCESSES -o $EDGEMICRO_ORG -e $EDGEMICRO_ENV -k $EDGEMICRO_KEY -s $EDGEMICRO_SECRET &"
-else
-  commandString="cd /opt/apigee && export EDGEMICRO_DECORATOR=$EDGEMICRO_DECORATOR && edgemicro start -o $EDGEMICRO_ORG -e $EDGEMICRO_ENV -k $EDGEMICRO_KEY -s $EDGEMICRO_SECRET &"
+	mgstart=" edgemicro start -o $EDGEMICRO_ORG -e $EDGEMICRO_ENV -k $EDGEMICRO_KEY -s $EDGEMICRO_SECRET -p $EDGEMICRO_PROCESSES -d /opt/apigee/plugins "
 fi
 
-#echo $commandString
-if [[ ${EDGEMICRO_SH} != "" ]]; then
-  su - apigee -s /bin/sh -c "$commandString"
-else 
-  su - apigee -c "$commandString"
+if [[ ${EDGEMICRO_LOCAL_PROXY} != "1" ]]; then
+  commandString="$mgdir && $mgstart $background"
+else
+  commandString="$mgdir && $decorator &&  $localproxy && $mgstart -a $proxy_name -v 1 -b / -t http://localhost:$target_port  $background"
 fi
+
+if [[ ${EDGEMICRO_DOCKER} != "" ]]; then
+  if [[ ${DEBUG} != "" ]]; then
+    su - apigee -s /bin/sh -c "$debug && $commandString"
+  else
+    su - apigee -s /bin/sh -c "$commandString"
+  fi
+else
+  if [[ ${DEBUG} != "" ]]; then
+    su - apigee -s /bin/sh -m -c "$debug && $commandString"
+  else 
+    su - apigee -s /bin/sh -m -c "$commandString"
+  fi
+fi 
 #edgemicro start &
 
 # SIGUSR1-handler
 my_handler() {
   echo "my_handler" >> /tmp/entrypoint.log
-  su - apigee -m -c "cd /opt/apigee && edgemicro stop"
-  if [[ ${EDGEMICRO_DECORATOR} != "" ]]; then
-      #Attempt deleting the proxy here
-      curl -v -X DELETE -u $EDGEMICRO_ADMINEMAIL:$EDGEMICRO_ADMINPASSWORD -H "Content-Type:application/x-www-form-urlencoded" ${EDGEMICRO_MGMTURL}/v1/organizations/${EDGEMICRO_ORG}/apiproducts/${product_name}
-      curl -v -X DELETE -u $EDGEMICRO_ADMINEMAIL:$EDGEMICRO_ADMINPASSWORD -H "Content-Type:application/x-www-form-urlencoded" ${EDGEMICRO_MGMTURL}/v1/organizations/${EDGEMICRO_ORG}/environments/${EDGEMICRO_ENV}/apis/${proxy_name}/revisions/1/deployments
-      curl -v -X DELETE -u $EDGEMICRO_ADMINEMAIL:$EDGEMICRO_ADMINPASSWORD -H "Content-Type:application/x-www-form-urlencoded" ${EDGEMICRO_MGMTURL}/v1/organizations/${EDGEMICRO_ORG}/apis/${proxy_name}
-  fi
-  #edgemicro stop
+  su - apigee -m -s /bin/sh -c "cd /opt/apigee && edgemicro stop"
 }
 
 # SIGTERM-handler
 term_handler() {
   echo "term_handler" >> /tmp/entrypoint.log
-  su - apigee -m -c "cd /opt/apigee && edgemicro stop"
-  if [[ ${EDGEMICRO_DECORATOR} != "" ]]; then
-      curl -v -X DELETE -u $EDGEMICRO_ADMINEMAIL:$EDGEMICRO_ADMINPASSWORD -H "Content-Type:application/x-www-form-urlencoded" ${EDGEMICRO_MGMTURL}/v1/organizations/${EDGEMICRO_ORG}/apiproducts/${product_name}
-      curl -v -X DELETE -u $EDGEMICRO_ADMINEMAIL:$EDGEMICRO_ADMINPASSWORD -H "Content-Type:application/x-www-form-urlencoded" ${EDGEMICRO_MGMTURL}/v1/organizations/${EDGEMICRO_ORG}/environments/${EDGEMICRO_ENV}/apis/${proxy_name}/revisions/1/deployments
-      curl -v -X DELETE -u $EDGEMICRO_ADMINEMAIL:$EDGEMICRO_ADMINPASSWORD -H "Content-Type:application/x-www-form-urlencoded" ${EDGEMICRO_MGMTURL}/v1/organizations/${EDGEMICRO_ORG}/apis/${proxy_name}
-  fi
-  #edgemicro stop
+  su - apigee -m -s /bin/sh -c "cd /opt/apigee && edgemicro stop"
   exit 143; # 128 + 15 -- SIGTERM
 }
 
@@ -77,3 +91,4 @@ while true
 do
         tail -f /dev/null & wait ${!}
 done
+
