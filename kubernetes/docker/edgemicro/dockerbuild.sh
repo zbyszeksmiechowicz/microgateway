247efe3eaa0f53363bf75490aa31c77b1af2ffc1
Keyur Karnik <keyur.karnik+github@gmail.com>
2019-09-29 07:03:33 -0700

    Modify docker script to release specific version

--- kubernetes/docker/edgemicro/dockerbuild.sh
+++ kubernetes/docker/edgemicro/dockerbuild.sh
@@ -3,7 +3,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ $# -ne 2 ]; then 
+if [ $# -ne 2 ]; then
 	echo "Please provide edgemicro version and GCP project id"
         exit 1
 fi
@@ -11,11 +11,17 @@ fi
 version=$1
 project_id=$2
 
-docker build -t edgemicro:$version $DIR
-
 if [ $# -eq 2 ]; then
+
+  sed -i .bak  "s/ *edgemicro.*/ edgemicro@$version/g" installnode.sh
+  docker build -t edgemicro:$version $DIR
   docker tag edgemicro:$version gcr.io/$project_id/edgemicro:$version
   docker tag edgemicro:$version gcr.io/$project_id/edgemicro:latest
   docker push gcr.io/$project_id/edgemicro:$version
   docker push gcr.io/$project_id/edgemicro:latest
+  rm installnode.sh
+  mv installnode.sh.bak installnode.sh
+
 fi
+
+
